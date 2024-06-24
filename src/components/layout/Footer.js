012266c0d95c0616da9fefd74e5fdb7cccf53372
Gabriel Raupp <gabiraupppimentel@gmail.com>
2024-06-24 16:54:51 -0300

    versão_02

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,17 +6,23 @@ function Footer() {
     <footer className={styles.footer}>
       <ul className={styles.social_list}>
         <li>
+          <a href='https://www.facebook.com/?locale=pt_BR'>
           <FaFacebook />
+          </a>
         </li>
         <li>
+          <a href='https://www.instagram.com/gabriel.raupp17/'>
           <FaInstagram />
+          </a>
         </li>
         <li>
+          <a href='https://www.linkedin.com/checkpoint/challenge/AgFw1s0up6YjiwAAAZBLshnxQjlnMVOY525QlcnAvM_jECGIVnmyMp8ifWbemZrPEXKhyxLzOVl0U9LahPvlX0wAryJjSA?ut=3rSFr8wbj9eHk1'>
           <FaLinkedin />
+          </a>
         </li>
       </ul>
       <p className={styles.copy_right}>
-        <span>Costs</span> &copy; 2021
+        <span>Costs</span> &copy; 2024
       </p>
     </footer>
   )
