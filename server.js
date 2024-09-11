02cb189546f7b3922b8de63c68c515a71251efb0
Gabriel Raupp <gabiraupppimentel@gmail.com>
2024-09-10 23:49:48 -0300

    ta tarde

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve o caminho do diretório atual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
